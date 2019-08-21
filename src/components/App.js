af763d1b87e699fc71922d3153246d41e323bceb
carterbancroft <carter.bancroft@gmail.com>
2019-08-20 22:46:28 -0800

    Cleanup and get a single cell rendering in react

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,16 @@
 import React from 'react'
-import Tone from 'tone'
+
+import Cell from './Cell'
 
 import './App.css'
 
-Tone.Transport.bpm.value = 120
 
-const synth = new Tone.Synth().toMaster()
 
 class App extends React.Component {
-  constructor() {
-    super(...arguments)
-
-    this.movePlayhead = this.movePlayhead.bind(this)
-  }
-
-  playSound() {
-    synth.triggerAttackRelease("C4", "8n")
-  }
-
-  playLoop() {
-    const loop = new Tone.Loop(function(time){
-      console.log('in')
-      synth.triggerAttackRelease("C2", "8n", time)
-    }, "4n")
-    loop.start("1m").stop("4m")
-
-    Tone.Transport.start()
-
-    document.querySelector(".playhead").bind(Tone.Transport)
-  }
-
-  movePlayhead() {
-  }
-
   render() {
     return (
       <div>
-        <button
-          className="btn btn-primary"
-          onClick={ this.playSound }>
-          Note
-        </button>
-
-        <button
-          className="btn btn-primary"
-          onClick={ this.playLoop }>
-          Loop
-        </button>
-
-        <div className="playbox">
-          <div className="playhead"/>
-        </div>
+        <Cell />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import { render } from 'react-dom'
 
 import App from './components/App'
 
+import './css/style.css'
+
 
 render(<App />, document.querySelector('#main'))

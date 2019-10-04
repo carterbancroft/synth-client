b9a76506f0d1edce4144f76c2395370b53634fc5
carterbancroft <carter.bancroft@gmail.com>
2019-10-03 23:02:20 -0400

    use instance vars and move individual synths into the instrument component

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import Tone from 'tone'
 
 import Instrument from './Instrument'
 
-let beat
-let bassSynth
 
 class App extends React.Component {
   state = {
@@ -15,29 +13,21 @@ class App extends React.Component {
   constructor(props) {
     super(props)
 
-    beat = new Tone.Loop(time => {
+    this.loop = new Tone.Loop(time => {
       const c = this.state.beat
       const b = (c + 1) % 16
 
       this.setState({ beat: b, time })
     }, '16n')
 
-    document.addEventListener('keydown', this.handleKeyPress);
-
     const bpm = 80
-
-    bassSynth = new Tone.MembraneSynth().toMaster()
 
     Tone.Transport.bpm.value = parseInt(bpm)
   }
 
-  play = () => {
-    bassSynth.triggerAttackRelease('C1', '8n', this.state.time)
-  }
-
-  handleClick = () => {
+  toggleLoop = () => {
     Tone.Transport.start()
-    beat.start(0)
+    this.loop.start(0)
   }
 
   // Add instruments here... State should have a list of instruments.
@@ -48,12 +38,12 @@ class App extends React.Component {
       instruments.push(<Instrument />)
     }*/
 
-    instruments.push(<Instrument play={this.play} currentBeat={this.state.beat} key={0} />)
+    instruments.push(<Instrument currentBeat={this.state.beat} key={0} />)
 
     return (
       <div>
         { instruments }
-        <button onClick={this.handleClick}>Loop</button>
+        <button onClick={this.toggleLoop}>Loop</button>
       </div>
     )
   }
--- src/components/Instrument.js
+++ src/components/Instrument.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import Tone from 'tone'
 
 import Cell from './Cell'
 
 
 class Instrument extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.synth = new Tone.MembraneSynth().toMaster()
+  }
+
+  play = () => {
+    this.synth.triggerAttackRelease('C1', '8n', this.props.currentTime)
+  }
+
   render() {
     const cells = []
 
     for (let i = 0; i < 16; i++) {
-      cells.push(<Cell play={this.props.play} key={ i } isPlaying={ this.props.currentBeat === i } />)
+      cells.push(<Cell play={this.play} key={ i } isPlaying={ this.props.currentBeat === i } />)
     }
 
     return (

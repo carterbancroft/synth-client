2901868a44085e7540582f1e7315b4a941395f4c
carterbancroft <carter.bancroft@gmail.com>
2019-10-15 16:55:27 -0400

    Remove the instrument adder for now

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,51 @@ import React from 'react'
 import Tone from 'tone'
 
 import Instrument from './Instrument'
-import InstrumentPicker from './InstrumentPicker'
+//import InstrumentPicker from './InstrumentPicker'
 
 
 class App extends React.Component {
   state = {
     beat: 0,
     time: null,
-    instruments: []
+    instruments: [
+      {
+        id: 0,
+        name: 'Bass',
+        config: {
+          synth: Tone.MembraneSynth,
+          attackRelease: {
+            note: 'C1',
+            duration: '8n',
+          },
+          key: 0,
+        },
+      },
+      {
+        id: 1,
+        name: 'High Hat',
+        config: {
+          synth: Tone.MetalSynth,
+          synthOptions: {
+            frequency: 250,
+            envelope: {
+              attack: 0.001,
+              decay: 0.1,
+              release: 0.01,
+            },
+            harmonicity: 3.1,
+            modulationIndex: 16,
+            resonance: 4000,
+            octaves: 1.5,
+          },
+          attackRelease: {
+            duration: '32n',
+            velocity: 0.3,
+          },
+          key: 1,
+        },
+      }
+    ]
   }
 
   constructor(props) {
@@ -53,13 +90,13 @@ class App extends React.Component {
         <button id="togglePlay" className="togglePlayButton" onClick={this.toggleLoop}>
           <i className="iconoo-play" />
         </button>
-        <InstrumentPicker key="instrumentPicker" addInstrument={this.addInstrument} />
+        { /*<InstrumentPicker key="instrumentPicker" addInstrument={this.addInstrument} />*/ }
         {
           this.state.instruments.map(instrument => {
             return <Instrument
-              synth={ instrument.synth }
-              synthOptions={ instrument.synthOptions || null }
-              attackRelease={ instrument.attackRelease }
+              synth={ instrument.config.synth }
+              synthOptions={ instrument.config.synthOptions || null }
+              attackRelease={ instrument.config.attackRelease }
               currentBeat={ this.state.beat }
               key={instrument.key}
             />

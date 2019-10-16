0118de9bdcc0efab133e889ae25c3f10805f8679
carterbancroft <carter.bancroft@gmail.com>
2019-10-16 15:00:07 -0400

    Store instruments in their own file instead of in state

--- src/components/InstrumentPicker.js
+++ src/components/InstrumentPicker.js
@@ -1,53 +1,12 @@
 import React from 'react'
-import Tone from 'tone'
+
+import instruments from '../instruments'
 
 
 class InstrumentPicker extends React.Component {
-  state = {
-    instruments: [
-      {
-        id: 0,
-        name: 'Bass',
-        config: {
-          synth: Tone.MembraneSynth,
-          attackRelease: {
-            note: 'C1',
-            duration: '8n',
-          },
-          key: 0,
-        },
-      },
-      {
-        id: 1,
-        name: 'High Hat',
-        config: {
-          synth: Tone.MetalSynth,
-          synthOptions: {
-            frequency: 250,
-            envelope: {
-              attack: 0.001,
-              decay: 0.1,
-              release: 0.01,
-            },
-            harmonicity: 3.1,
-            modulationIndex: 16,
-            resonance: 4000,
-            octaves: 1.5,
-          },
-          attackRelease: {
-            duration: '32n',
-            velocity: 0.3,
-          },
-          key: 1,
-        },
-      }
-    ],
-  }
-
   toggleInstrument = e => {
     const name = e.target.value
     const isChecked = e.target.checked
-    const { instruments } = this.state
     const instrument = instruments.find(i => i.name === name)
 
     isChecked ?
@@ -56,15 +15,11 @@ class InstrumentPicker extends React.Component {
   }
 
   componentDidMount() {
-    const { instruments } = this.state
-
     // Add all the instruments on startup
     instruments.forEach(i => this.props.addInstrument(i))
   }
 
   render() {
-    const { instruments } = this.state
-
     const checkboxes = instruments.map(instrument => {
       return (
         <li key={ instrument.id }>

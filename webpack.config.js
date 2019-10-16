8a5d1713ecaf2a3ee56d0141d80b10467503e855
carterbancroft <carter.bancroft@gmail.com>
2019-10-16 13:01:11 -0400

    Switch over to less and get it working

--- webpack.config.js
+++ webpack.config.js
@@ -12,19 +12,21 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: { loader: 'babel-loader' },
       },
       {
         test: /\.css$/,
         use: [
-          {
-            loader: 'style-loader', // creates style nodes from JS strings
-          },
-          {
-            loader: 'css-loader', // translates CSS into CommonJS
-          },
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'less-loader' },
         ],
       },
     ]
